b7d33b0dea058f8f58c8d596d25db270ebf3464e
patrick borowy <patrick.borowy@protonmail.com>
2024-10-14 13:44:56 +0200

    prepare changes for tag del

--- src/lab_initialization.py
+++ src/lab_initialization.py
@@ -6,14 +6,7 @@ from argostranslate import translate as _translate
 
 def lab_initialization():
     device = torch.cuda.current_device() if torch.cuda.is_available() else -1
-    classifier = pipeline(
-        "zero-shot-classification",
-        model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli",
-        device=device,
-        batch_size=16,
-        top_k=None,
-        max_length=64,
-    )
+    
     labels = requests.get(
         "https://raw.githubusercontent.com/exorde-labs/TestnetProtocol/main/targets/class_names.json"
     ).json()
@@ -21,7 +14,6 @@ def lab_initialization():
     installed_languages = _translate.get_installed_languages()
     return {
         "device": device,
-        "classifier": classifier,
         "labeldict": labels,
         "max_depth": 2,
         "remove_stopwords": False,
